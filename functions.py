446d788100322dd7afe204e27f5c1ca32b82b75e
1lec <alecandersen97@gmail.com>
2023-06-08 12:32:40 -0400

    Created round_robin_classes.py, which contains two classes: Player and Round.
    Created functions.py, which contains the generate_rounds function.

---
+++ functions.py
@@ -0,0 +1,20 @@
+from round_robin_classes import Round
+
+def generate_rounds(players_list):
+    """Takes a list of players as an argument and returns a dictionary with round numbers as keys and round objects
+    as values. The number of key-value pairs is equivalent to one less than the number of players."""
+
+    number_of_rounds = len(players_list) - 1
+
+    round_dict = {}
+
+    count = 0
+    while count < number_of_rounds:
+        round_dict[count + 1] = Round(count + 1)
+        count += 1
+
+    return round_dict
+
+# players_list = ['Caruana', 'Carlsen', 'Nakamura', 'Tari', 'So', 'Shankland', 'Kasparov', 'Karpov']
+
+# tournament_rounds = generate_rounds(players_list)
---
+++ round_robin_classes.py
@@ -0,0 +1,57 @@
+class Player:
+    """Represents a player in a chess tournament with 3 private data members: name, previous_opponents,
+    and previous_colors."""
+
+    def __init__(self, name):
+        self._name = name
+        self._previous_opponents = set()  # Using a set to eliminate potential for duplicates.
+        self._previous_colors = []  # Colors are sorted from least to most recently played.
+
+    def get_name(self):
+        """Returns the name of a player."""
+
+        return self._name
+
+    def get_previous_opponents(self):
+        """Returns the previous opponents of a player."""
+
+        return self._previous_opponents
+
+    def get_previous_colors(self):
+        """Returns the previous colors the player has had."""
+
+        return self._previous_colors
+
+    def add_opponent(self, player_object):
+        """Takes a player object as an argument and adds it to a different player's list of previous opponents."""
+
+        self._previous_opponents.add(player_object)
+
+    def add_color(self, color):
+        """Takes a string as an argument and appends it to the players list of previous colors."""
+
+        self._previous_colors.append(color)
+
+
+class Round:
+    """Represents a round in a chess tournament with two private data members: round_number and pairings."""
+
+    def __init__(self, round_number):
+
+        self._round_number = round_number
+        self._pairings = set()  # Using a set to eliminate potential for duplicates.
+
+    def get_round_number(self):
+        """Returns the round number of a round object."""
+
+        return self._round_number
+
+    def get_pairings(self):
+        """Returns the pairings for a round."""
+
+        return self._pairings
+
+    def add_pairing(self, pairing):
+        """Takes a pairing as an argument and adds it to the set of pairings for the round."""
+
+        self._pairings.add(pairing)
