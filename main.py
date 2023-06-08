0b33cef25eebde9ff2c5079b97cfb6356e721d11
1lec <alecandersen97@gmail.com>
2023-06-08 17:28:44 -0400

    Expanded pseudocode, but ran into an issue. I was hoping to be able to reorder the dictionary of players in the event that acceptable pairings weren't found, but apparently dictionaries cannot be reordered. I am considering changing the dictionary of Player Objects into a list of Player objects so that the order can be changed.

--- main.py
+++ main.py
@@ -12,4 +12,15 @@
 # player if the number of rounds they've been paired is less than the current round that is being paired, find an
 # unpaired opponent for the player by again looping through the dictionary of players.
 
-# Step 5: Once an unpaired opponent is found for the player, determine the colors.
+# Step 5: Once an unpaired opponent is found for the player, determine the colors by comparing the number of whites
+# played by both players. If they have played the same number of whites, compare their most recent colors played, loop
+# through their list of colors played until a difference is found. Once a difference is found, give white to player
+# who played black. If the players have played the exact same number of whites in the same order, give a random player
+# white.
+
+# Step 6: Once a pairing is determined, create a tuple in the form of (white_player, black_player) and add it to the
+# set of pairings contained within the current Round object. Add 1 to the rounds_played for both players, and update
+# each player's colors_played and white_count accordingly.
+
+# Step 7: If no unpaired opponent was found for a player, empty the set of pairings for the current Round object,
+# shuffle the dictionary of players
