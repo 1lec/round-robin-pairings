53ccfe2686002f9e4f5f850f0c5b487ca03f8fcc
1lec <alecandersen97@gmail.com>
2023-08-22 15:12:07 -0400

    Removed print statement from main.py.

--- main.py
+++ main.py
@@ -3,7 +3,3 @@ from classes import *
 ASU_Open = Tournament('ASU Open', 'August 22nd, 2023', 'Tempe, Arizona')
 ASU_Open.pair_single_round_robin()
 round_dict = ASU_Open.get_round_dict()
-
-# print test
-for round_num in round_dict:
-    print(round_dict[round_num].get_pairings())
