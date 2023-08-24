e98c19378db6258ebeae4a1fd0f3832724eaefc7
1lec <alecandersen97@gmail.com>
2023-08-24 11:54:09 -0400

    Removed long pseudocode in classes.py.

--- classes.py
+++ classes.py
@@ -220,10 +220,6 @@ class Tournament:
             self._round_dict[count] = Round(count)
             count += 1
 
-        # Step 5: Loop through the dictionary of rounds, and for each round, loop through the list of Player objects.
-        # For each Player, if the number of rounds they've been paired is less than the current round that is being
-        # paired, find an unpaired opponent for the player by again looping through the list of Player objects.
-
         for round_num in self._round_dict:  # loop through each round in the dictionary of rounds
             while self._round_dict[round_num].is_incomplete(player_object_list):
                 for player_1 in player_object_list:  # for each round, find an unpaired player in player_object_list
@@ -248,26 +244,3 @@ class Tournament:
                 for pairing in self._round_dict[round_num].get_pairings():
                     outfile.write(pairing[0] + ' - ' + pairing[1] + '\n')
                 outfile.write('\n')
-
-# Step 6: Once an unpaired opponent is found for the player, determine the colors by comparing the number of whites
-# played by both players. If they have played the same number of whites, compare their most recent colors played, loop
-# through their list of colors played until a difference is found. Once a difference is found, give white to player
-# who played black. If the players have played the exact same number of whites in the same order, give a random player
-# white.
-
-# Step 7: Once a pairing is determined, create a tuple in the form of (white_player, black_player) and add it to the
-# set of pairings contained within the current Round object. Add 1 to the rounds_played for both players, and update
-# each player's colors_played and white_count accordingly.
-
-# Step 8a: If no unpaired opponent was found for a player, the round must be repaired, which requires a few steps to
-# reset. First, for those players who were already paired (and thus need to be repaired), remove the last player in
-# their list of previous opponents, remove their most recently played color (and if that color was white, subtract 1
-# from their white_count), and reset their number of rounds paired to the previous round. Finally, empty the set of
-# pairings for the current Round object.
-
-# Step 8b: Once the invalid pairings from Step 7 are reset, shuffle the list of Player objects and reiterate through
-# the Round. Continue this step until a valid set of pairings is obtained, returning to Step 8a as needed. A set of
-# pairings is valid once the length of set is equal to half the number of Player objects.
-
-# Step 9: Once a valid set of pairings is obtained for every Round object, write the round number and corresponding
-# pairings into a new file.
