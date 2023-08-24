70a94f231177d13f2abca8b59c723f367cbe6c33
1lec <alecandersen97@gmail.com>
2023-08-24 10:34:03 -0400

    Further simplified logic in pair_single_round_robin method.

--- classes.py
+++ classes.py
@@ -227,15 +227,13 @@ class Tournament:
         for round_num in self._round_dict:  # loop through each round in the dictionary of rounds
             while self._round_dict[round_num].is_incomplete(player_object_list):
                 for player_1 in player_object_list:  # for each round, find an unpaired player in player_object_list
-                    paired = player_1.get_rounds_paired() == round_num
-                    if not paired:
+                    if player_1.get_rounds_paired() < round_num:
                         for player_2 in player_object_list:
                             if player_1.is_valid_opponent(player_2, round_num):
                                 player_1.determine_colors(player_2)
                                 self._round_dict[round_num].generate_pairing(player_1, player_2)
-                                paired = True
                                 break
-                        if not paired:
+                        if player_1.get_rounds_paired() < round_num:
                             self._round_dict[round_num].reset_round(player_object_list)
                             random.shuffle(player_object_list)
 
